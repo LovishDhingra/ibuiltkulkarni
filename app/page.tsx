c7c9b18e492fd7d757939b296ae2c7008e67bf21
Lovish Dhingra <lovishdhingra2023@gmail.com>
2026-06-27 23:21:33 +0530

    day7

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import HeroSection from "@/components/ui/landing-page/hero-section";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-full p-8">
-      <h1 className="text-4xl font-bold mb-4">Welcome to My Site</h1>
-      <p className="text-lg text-gray-600 mb-8">Built with Next.js and Tailwind CSS</p>
-    </div>
+     <div>
+   <HeroSection />
+     </div>
+
   );
 }
